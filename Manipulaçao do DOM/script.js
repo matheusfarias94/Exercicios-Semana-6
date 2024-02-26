d5272ecb61dc69b7f69fb7d3fee1049a9f41d3d7
Matheus Farias <matheusfarias95@hotmail.com>
2024-02-26 20:24:44 -0300

    new file:   "Manipula\303\247ao do DOM/index.html" 	new file:   "Manipula\303\247ao do DOM/script.js" 	new file:   "Manipula\303\247ao do DOM/style.css"

---
+++ Manipulaçao do DOM/script.js
@@ -0,0 +1,37 @@
+
+function atualizarNome() {
+    let nome = prompt("Qual é o seu nome?");
+    let sobre = prompt('Fale sobre voce');
+    let mail = prompt('Digite seu e-mail');
+    let tel = prompt('Digite seu telefone');
+    let endereço = prompt('Digite seu endereço');
+
+    
+
+    if (nome !== null) {
+        document.getElementById('nomeUsuario').textContent = nome;
+    }
+
+    if (sobre !== null) {
+        document.getElementById('sobreMim').textContent = sobre;
+
+    }
+
+    if (mail !== null) {
+        document.getElementById('email').textContent = mail;
+
+    }
+    if (tel !== null) {
+        document.getElementById('telefone').textContent = tel;
+
+    }
+    if (endereço !== null) {
+        document.getElementById('local').textContent = endereço;
+
+    }
+    
+
+
+
+
+}
